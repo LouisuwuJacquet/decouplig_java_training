bb36eaa57fb9cbc7b814b85be5cb31f5aa19adb7
LouisuwuJacquet <ljacquet@et.esiea.fr>
2021-12-02 15:03:34 +0100

    Creation of the 7th part : finished

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,8 +1,10 @@
 package fr.lernejo.logger;
 import java.nio.file.Paths;
+import java.util.function.Predicate;
 
 public class LoggerFactory {
     public static Logger getLogger(String name){
-        return new CompositeLogger(new ContextualLogger(name,new ConsoleLogger()), new ContextualLogger(name,new FileLogger(Paths.get("captain.log").toString())));
+        Predicate<String> condtion = message -> message.contains("simulation");
+        return new CompositeLogger(new ContextualLogger(name,new ConsoleLogger()),new ContextualLogger(name,new FilteredLogger(new FileLogger(Paths.get("captain.log").toString()),condtion)));
     }
 }
