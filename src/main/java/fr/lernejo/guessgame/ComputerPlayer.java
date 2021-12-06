9d7f5828e8ff65a546ea89a23af60884f683f0d0
LouisuwuJacquet <ljacquet@et.esiea.fr>
2021-12-06 14:34:38 +0100

    Computer Player modified

--- src/main/java/fr/lernejo/guessgame/ComputerPlayer.java
+++ src/main/java/fr/lernejo/guessgame/ComputerPlayer.java
@@ -6,20 +6,34 @@ import java.security.SecureRandom;
 
 public class ComputerPlayer implements Player {
     private final Logger logger = LoggerFactory.getLogger("computer");
+    private boolean isFirstGuess = true;
+    private boolean previousLowerOrGreater;
+    private long min = 1;
+    private long max = 100;
 
     @Override
     public long askNextGuess() {
-        SecureRandom random = new SecureRandom();
-        long randomNumber = random.nextLong(1000);
-        logger.log(""+randomNumber);
-        return randomNumber;
+        if (!isFirstGuess) {
+            if (previousLowerOrGreater) {
+                this.min = (this.min + this.max) / 2;
+            } else {
+                this.max = (this.min + this.max) / 2;
+            }
+        } else {
+            this.isFirstGuess = false;
+        }
+        long guess = (this.min + this.max) / 2;
+        logger.log("Le joueur a choisit " + guess);
+        return guess;
     }
 
     @Override
     public void respond(boolean lowerOrGreater) {
-        //cd n'est pas une question de savoir si il est dans l'interval, mais si il est trop petit(cas1) ou trop grand(cas2)
-
+        if (lowerOrGreater) {
+            this.logger.log("Please, choose a greater number");
+        } else {
+            this.logger.log("Please, choose a lesser number");
+        }
+        previousLowerOrGreater = lowerOrGreater;
     }
-
-
 }
